3932119ee7d810cb3db5dfdf10e1126a9064dbb4
Divaka Tharinda Perera <thimiradivaka@gmail.com>
2023-10-30 11:32:00 +0530

    spring boot app initialized

---
+++ BeMyVoice-backend/src/main/java/com/bemyvoice/bemyvoicebackend/BeMyVoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bemyvoice.bemyvoicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zBeMyVoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMyVoiceBackendApplication.class, args);
+	}
+
+}
