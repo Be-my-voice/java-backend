5f499d37b6820a5e4ee1b00b93002a4cf80f0859
Divaka Tharinda Perera <thimiradivaka@gmail.com>
2023-10-30 11:58:14 +0530

    lesson cruds started

--- BeMyVoice-backend/src/main/java/com/bemyvoice/bemyvoicebackend/BeMyVoiceBackendApplication.java
+++ BeMyVoice-backend/src/main/java/com/bemyvoice/bemyvoicebackend/BeMyVoiceBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class zBeMyVoiceBackendApplication {
+public class BeMyVoiceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeMyVoiceBackendApplication.class, args);
